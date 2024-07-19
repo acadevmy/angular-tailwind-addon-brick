7361931c8ba0af64718e7b9b460b3fd7f6bfe8b8
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-07-19 11:10:31 +0200

    spyke: test update styles.scss file

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -1,3 +1,13 @@
+import 'dart:io';
 import 'package:mason/mason.dart';
 
-void run(HookContext context) async {}
+void run(HookContext context) async {
+  await updateStyleFile(context: context);
+}
+
+Future<void> updateStyleFile({required HookContext context}) async {
+  await await Process.run(
+    'sed',
+    ['-i', "'1s/^/@import tailwind;\n/'", 'src/styles.scss'],
+  );
+}
