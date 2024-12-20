292dd443cf3264350d90e7223dd85f81a604c0d1
Gabriele Bilello <gabriele.bilello1@gmail.com>
2024-12-20 15:45:28 +0100

    chore: migrate to new import syntax

--- hooks/post_gen.dart
+++ hooks/post_gen.dart
@@ -9,7 +9,7 @@ Future<void> updateStyleFile({required HookContext context}) async {
   final file = File("src/styles.scss");
   String originalContent = await file.readAsString();
 
-  String tailwindImport = "@import 'tailwind';";
+  String tailwindImport = '@use "../src/assets/scss/tailwind.scss";';
   String newContent = tailwindImport + '\n' + originalContent;
 
   await file.writeAsString(newContent);
